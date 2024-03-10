df1201288dac09a74f9eee6db8d53c6fb4f17693
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-10 14:58:04 -0600

    Configuracion de la base de datos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+config();
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot(
+    {
+      type: 'mysql',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [],
+      synchronize: true,
+    }
+  ),UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
