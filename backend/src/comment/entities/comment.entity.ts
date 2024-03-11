039af2c76fa041fae1762f09ce404f2e547a3a4d
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-11 08:23:30 -0600

    Propiedades agregadas a la entidad Comment

--- backend/src/comment/entities/comment.entity.ts
+++ backend/src/comment/entities/comment.entity.ts
@@ -1,1 +1,14 @@
-export class Comment {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Comment {
+    @PrimaryGeneratedColumn()
+    id: number;
+    postId: number;
+    @Column()
+    content: string;
+    userId: number;
+    commentStatus: any;
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+}
