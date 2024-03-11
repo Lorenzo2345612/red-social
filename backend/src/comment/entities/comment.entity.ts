d0dc7b03b7cc51dce4dd42b833a15eef1513c2c1
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-11 08:34:11 -0600

    Comment-Post relationship added

--- backend/src/comment/entities/comment.entity.ts
+++ backend/src/comment/entities/comment.entity.ts
@@ -1,9 +1,11 @@
+import { Post } from "src/post/entities/post.entity";
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Comment {
     @PrimaryGeneratedColumn()
     id: number;
+    @ManyToOne(() => Post, post => post.comments)
     postId: number;
     @Column()
     content: string;
