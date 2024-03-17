d0bab75632760f6778366458b1c0ac118d83c4f3
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2024-03-17 16:25:16 -0600

    Delete backend

--- backend/src/post-view/dto/update-post-view.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostViewDto } from './create-post-view.dto';
-
-export class UpdatePostViewDto extends PartialType(CreatePostViewDto) {}
--- backend/src/post-view/post-view.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostViewService } from './post-view.service';
-import { PostViewController } from './post-view.controller';
-
-@Module({
-  controllers: [PostViewController],
-  providers: [PostViewService],
-})
-export class PostViewModule {}
